511b5de9cd37719a28b068d959e95344b9d629c6
jesus alejandro varela castro <yakorox9@gmail.com>
2019-12-03 00:44:58 -0400

    penultimo

--- src/app/interface/employees.ts
+++ src/app/interface/employees.ts
@@ -1,8 +1,17 @@
 import {Employee} from './employee'
 export const Employees: Employee[] =[
-    {id: "1", name: "Simon Araque", value: 1, url:"8" , isActive: false},
-    {id: "2", name: "Victor Sanchez", value: 2,  url:"9" ,isActive: false},
-    {id: "3", name: "David Flaco", value: 3, url:"2" , isActive: false},
-    {id: "4", name: "Ivan MePerdonas", value: 4, url:"5" , isActive: false},
-    {id: "5", name: "Marco Che Guevara", value: 5, url:"6" , isActive: false}
+    {id: "1", name: "Simon Araque", value: 0, url:"8" , isActive: false},
+    {id: "2", name: "Victor Sanchez", value: 0,  url:"9" ,isActive: false},
+    {id: "3", name: "David Delgado", value: 0, url:"2" , isActive: false},
+    {id: "4", name: "Ivan Fuentes", value: 0, url:"5" , isActive: false},
+    {id: "5", name: "Marco Useche", value: 0, url:"6" , isActive: false},
+    {id: "6", name: "Luis Vargas", value: 0,  url:"4" ,isActive: false},
+    {id: "7", name: "Mario Grieco", value: 0, url:"7" , isActive: false},
+    {id: "8", name: "Alexander ", value: 0, url:"0" , isActive: false},
+    {id: "9", name: "Hilmer ", value: 0, url:"0" , isActive: false},
+    {id: "10", name: "Alejandro Vargas", value: 0,  url:"0" ,isActive: false},
+    {id: "11", name: "Blas Paredes", value: 0, url:"0" , isActive: false},
+    {id: "12", name: "Leandro Casique", value: 0, url:"0" , isActive: false},
+    {id: "13", name: "Brayan ", value: 0, url:"1" , isActive: false},
+    {id: "14", name: "Francisco Rubial", value: 0,  url:"3" ,isActive: false}
 ]
